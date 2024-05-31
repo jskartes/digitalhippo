19d4b7e5f0db2cce2188aefd27053797b5fb5cdf
jskartes <joshuakartes@gmail.com>
2024-05-31 15:09:20 -0600

    connect resend to sign-up process; confirm e-mail sent

--- src/getPayloadClient.ts
+++ src/getPayloadClient.ts
@@ -1,4 +1,5 @@
 import dotenv from "dotenv";
+import nodemailer from "nodemailer";
 import path from "path";
 import payload, { Payload } from "payload";
 import type { InitOptions } from "payload/config";
@@ -6,6 +7,16 @@ import type { InitOptions } from "payload/config";
 interface Args { initOptions?: Partial<InitOptions> }
 
 dotenv.config({ path: path.resolve(__dirname, "../.env") });
+
+const transport = nodemailer.createTransport({
+  host: "smtp.resend.com",
+  secure: true,
+  port: 465,
+  auth: {
+    user: "resend",
+    pass: process.env.RESEND_API_KEY
+  }
+});
 
 let cachedPayloadClient = (global as any).payload;
 if (!cachedPayloadClient)
@@ -15,6 +26,11 @@ export const getPayloadClient = async ({ initOptions }: Args = {}): Promise<Payl
   if (!process.env.PAYLOAD_SECRET) throw new Error("PAYLOAD_SECRET is missing from .env");
   if (cachedPayloadClient.client) return cachedPayloadClient.client;
   if (!cachedPayloadClient.promise) cachedPayloadClient.promise = payload.init({
+    email: {
+      transport,
+      fromAddress: "onboarding@resend.dev",
+      fromName: "DigitalHippo"
+    },
     secret: process.env.PAYLOAD_SECRET,
     local: initOptions?.express ? false : true,
     ...(initOptions || {})
