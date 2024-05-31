24f577a101f600e35e13cf54d1355453263d00dc
jskartes <joshuakartes@gmail.com>
2024-05-31 11:33:30 -0600

    add user sign-in/sign-up links to Navbar; format Cart contents

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,22 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export const formatPrice = (
+  price: number | string,
+  options: {
+    currency?: "USD" | "CAD" | "EUR" | "GBP";
+    notation?: Intl.NumberFormatOptions["notation"];
+  } = {}
+) => {
+  const { currency = "USD", notation = "compact" } = options;
+  const numericPrice = typeof price === "string" ? parseFloat(price) : price;
+
+  return new Intl.NumberFormat(
+    "en-US",
+    { style: "currency", currency, notation, maximumFractionDigits: 2 }
+  ).format(numericPrice);
 }
