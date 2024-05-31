225f0981720b12ceff0de4b4e03a6db3fbf35dda
jskartes <joshuakartes@gmail.com>
2024-05-31 13:03:34 -0600

    set up express server, payload CMS, MongoDB

---
+++ src/getPayloadClient.ts
@@ -0,0 +1,31 @@
+import dotenv from "dotenv";
+import path from "path";
+import payload from "payload";
+import type { InitOptions } from "payload/config";
+
+interface Args { initOptions?: Partial<InitOptions> }
+
+dotenv.config({ path: path.resolve(__dirname, "../.env") });
+
+let cachedPayloadClient = (global as any).payload;
+if (!cachedPayloadClient)
+  cachedPayloadClient = (global as any).payload = { client: null, promise: null };
+
+export const getPayloadClient = async ({ initOptions }: Args = {}) => {
+  if (!process.env.PAYLOAD_SECRET) throw new Error("PAYLOAD_SECRET is missing from .env");
+  if (cachedPayloadClient.client) return cachedPayloadClient.client;
+  if (!cachedPayloadClient.promise) cachedPayloadClient.promise = payload.init({
+    secret: process.env.PAYLOAD_SECRET,
+    local: initOptions?.express ? false : true,
+    ...(initOptions || {})
+  });
+
+  try {
+    cachedPayloadClient.client = await cachedPayloadClient.promise;
+  } catch (error: unknown) {
+    cachedPayloadClient.promise = null;
+    throw error;
+  }
+
+  return cachedPayloadClient.client;
+}
