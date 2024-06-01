fc8e7d2b0b25601d2feb77b7f5b7ccc8455258f4
jskartes <joshuakartes@gmail.com>
2024-05-31 20:19:51 -0600

    begin displaying products on front page, using Swiper

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+import ProductReel from "@/components/ProductReel";
 import { Button, buttonVariants } from "@/components/ui/button";
 import { ArrowDownToLine, ArrowRight, CheckCircle, Leaf } from "lucide-react";
 import Link from "next/link";
@@ -48,7 +49,11 @@ const Page = () => {
             </Button>
           </div>
         </div>
-        {/* TODO: List products */}
+        <ProductReel
+          title="Brand new"
+          href="/products"
+          query={{ sort: "desc", limit: 4 }}
+        />
       </MaxWidthWrapper>
       <section className="border-t border-gray-200 bg-gray-50">
         <MaxWidthWrapper className="py-20">
